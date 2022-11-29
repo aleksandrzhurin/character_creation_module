ca147ebd1fa6b9c72b12ad437390dfe64f58fa26
Стас Басов <stas.basov@whatever.com>
2022-11-29 20:37:09 +0300

    Second commit

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str):
+    """Privet."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -15,6 +16,7 @@ def attack(char_name: str, char_class: str):
 
 
 def defence(char_name: str, char_class: str):
+    """Privedt."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -24,6 +26,7 @@ def defence(char_name: str, char_class: str):
 
 
 def special(char_name: str, char_class: str):
+    """Privet."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -34,6 +37,7 @@ def special(char_name: str, char_class: str):
 
 
 def start_training(char_name: str, char_class: str):
+    """Privet."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -58,6 +62,7 @@ def start_training(char_name: str, char_class: str):
 
 
 def choice_char_class():
+    """Privet."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -80,6 +85,7 @@ def choice_char_class():
 
 
 def main():
+    """Privet."""
     if __name__ == '__main__':
         run_screensaver()
         print('Приветствую тебя, искатель приключений!')
